731aa8239797fa71b86a13e189034dc63af9ba27
blackop166179 <tah9216@icloud.com>
2026-02-27 21:42:31 +0000

    began setup for arduino connection  code

---
+++ Rasberry Pi/Tengen_Nervous.py
@@ -0,0 +1,8 @@
+import json
+from pathlib import Path
+
+data_folder = Path ("Rasberry Pi/Memory")
+Tengen_Memory = data_folder / "Tengen_Memory.json"
+
+#this will be how data is sent from pi to arduino. 
+
